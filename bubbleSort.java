b859859dbe5e9a25797a3916a5c61710e71e3965
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2025-10-05 23:23:38 +0530

    Bubble sorting

---
+++ bubbleSort.java
@@ -0,0 +1,39 @@
+public class bubbleSort {
+   
+    public static void main(String[] args) {
+        int[] arr = {5, 1, 4, 2, 8}; 
+
+        System.out.println("Before Sorting:");
+        printArray(arr);
+
+        bubbleSort(arr); 
+
+        System.out.println("\nAfter Sorting:");
+        printArray(arr);
+    }
+
+    
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+
+    public static void printArray(int[] arr) {
+        for (int value : arr) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+}
+
+
