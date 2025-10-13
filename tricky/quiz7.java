fabcfe462e777ca294070af1e8f6a219ac94f50c
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2025-10-13 13:01:22 +0530

    updated

--- tricky/quiz7.java
+++ tricky/quiz7.java
@@ -1,7 +1,7 @@
 public class quiz7 {
     public static void main(String[] args){
-        String str1 = "Java";
-        String str2 = ("Java");
+        String str1 = "Max";
+        String str2 = ("Max");
         System.out.print(str1 == str2);
       
     }
