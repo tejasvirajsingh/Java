11836e1ca1147de68e04483b401ab64f4b011710
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2025-10-13 12:59:33 +0530

    updated

--- tricky/quiz6.java
+++ tricky/quiz6.java
@@ -1,6 +1,6 @@
 public class quiz6 {
     public static void main(String[] args)
     {
-        System.out.println('M' + 'I' + 'r' + 'z' + 'a' +  'p' + 'u' + 'r');
+        System.out.println('H' + 'A' + 'R' + 'S' + 'H');
     }
 }
