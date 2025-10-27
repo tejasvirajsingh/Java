9e9bdb9deaaf60e3e7925892ca556fd46dc59753
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2025-10-27 10:08:22 +0530

    updated

--- nextFive.java
+++ nextFive.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class nextFive {
     public static void main (String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the Character");
+        System.out.println("Enter the Characters");
         char ch = sc.next().charAt(0);
         for(int i = 0; i<5 ; i++)
         {
