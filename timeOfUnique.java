2e214eafc7749a78942f582f6561c260830f0fc6
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2025-11-04 22:40:16 +0530

    update

---
+++ timeOfUnique.java
@@ -0,0 +1,13 @@
+public class timeOfUnique {
+    public static void main(String[] args) {
+        
+        
+        // Get the current time in milliseconds since January 1, 1970
+
+
+        long uniqueTime = System.currentTimeMillis();
+
+        System.out.println("Here is a unique number (current milliseconds):");
+        System.out.println(uniqueTime);
+    }
+}
